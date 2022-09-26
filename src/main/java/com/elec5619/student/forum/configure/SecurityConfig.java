23a99b175ec8623e8764a056d8bfb381b111bfd5
Zihan Zheng <34792221+ZihanZheng258@users.noreply.github.com>
2022-09-26 17:56:30 +0800

    Update SecurityConfig.java

--- src/main/java/com/elec5619/student/forum/configure/SecurityConfig.java
+++ src/main/java/com/elec5619/student/forum/configure/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +20,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    protected  void configure(HttpSecurity http) throws Exception {
+          http.logout().logoutUrl("/url").logoutSuccessUrl("/url").permitAll();
+          http.exceptionHandling().accessDeniedPage("/unauthUrl");
+          http.formLogin().loginPage("/loginurl").loginProcessingUrl("/loginProcessorUrl")
+                  .defaultSuccessUrl("successURL").permitAll()
+                  .and().authorizeHttpRequests().antMatchers("").permitAll()
+                  .anyRequest().authenticated().and().csrf().disable();
+
     }
 
     @Bean
