87a366aeea91f502a380d5533d7c891877069840
FCS5 <cjhs50000@gmail.com>
2022-09-29 20:54:21 +1000

    merge

--- forum_client/src/components/DiscussionList/index.jsx
+++ forum_client/src/components/DiscussionList/index.jsx
@@ -30,26 +30,7 @@ const DiscussionList = ()=>{
     // setCardContent(Array.from(props))
     console.log(cardContent)
     const arr = [1,2,3,4]
-    const [data] = useState([
-        {
-            userId: '01a',
-            comId: '012',
-            fullName: 'Riya Negi',
-            avatarUrl: 'https://ui-avatars.com/api/name=Riya&background=random',
-            userProfile: 'https://www.linkedin.com/in/riya-negi-8879631a9/',
-            text: 'Hey, Loved your blog! ',
-            replies: [
-                {
-                    userId: '02b',
-                    comId: '017',
-                    fullName: 'Lily',
-                    userProfile: 'https://www.linkedin.com/in/riya-negi-8879631a9/',
-                    text: 'I have a doubt about the 4th point🤔',
-                    avatarUrl: 'https://ui-avatars.com/api/name=Lily&background=random',
-                    replies: []
-                }
-            ]
-        }])
+
 
     return(
         <div className="discussion-list">
