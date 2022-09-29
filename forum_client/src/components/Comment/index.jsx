6531527b24a7c7d59c9204155681fa9ad3bab721
FCS5 <cjhs50000@gmail.com>
2022-09-29 19:33:17 +1000

    comment structure done

--- forum_client/src/components/Comment/index.jsx
+++ forum_client/src/components/Comment/index.jsx
@@ -1,7 +1,12 @@
 import React, {useEffect} from "react";
 import './index.less'
+import commentImg from './assets/comment.svg'
+import avatar from './assets/avatar.jpeg'
+import {Link} from "react-router-dom";
 
-const Comment =() =>{
+const Comment =(props) =>{
+    // const commentClick
+
     // fetch the content
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/posts?_limit=10',
@@ -12,7 +17,57 @@ const Comment =() =>{
 
     return(
         <>
+            <div className="commentBlock">
+                <div className="comment-action" onClick={()=>{}}>
+                    <img src= {commentImg} alt=""/> <span>22 Comments</span>
+                </div>
+                <div className="comment-expand">
+                    <div className="comment">
+                        <div className="mainCommenter">
+                            <div className="commenterInfo">
+                                <img className="avatar" src={avatar} alt=""/>
+                                <span className="commentUserName">userNameCoco</span>
+                            </div>
+                            <div className="mainCommenterContent">
+                                <div className="mainCommenterContent-content">
+                                    comment content
+                                    quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit mole
+                                    stiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto
+                                </div>
+                                <div className="mainCommenterContent-dateReply">
+                                    <span className="date">29/09/2022 </span>
+                                    <Link to="">
+                                        <span>reply</span>
+                                    </Link>
+                                </div>
+                                <div className="subCommenter">
+                                    <div className="subCommenterContent">
+                                        <div>
+                                            <img className="avatar" src={avatar} alt=""/>
+                                            <span className="commentUserName">userNameCisco reply userNameCoco</span>
+                                        </div>
+                                        <div className="subCommenterContent-content">
+                                            comment content
+                                            quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit mole
+                                            stiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto
+                                            <div className="subCommenterContent-dateReply">
+                                                <span className="date">29/09/2022 </span>
+                                                <Link to="">
+                                                    <span>reply</span>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
 
+                                </div>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
 
         </>
     )
