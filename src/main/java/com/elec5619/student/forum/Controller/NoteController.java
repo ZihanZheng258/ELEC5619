b84154b4da9c2798043fdeedf1352687d6568fb5
Zihan Zheng <34792221+ZihanZheng258@users.noreply.github.com>
2022-10-06 11:06:20 +0800

    file

--- src/main/java/com/elec5619/student/forum/Controller/NoteController.java
+++ src/main/java/com/elec5619/student/forum/Controller/NoteController.java
@@ -7,13 +7,28 @@ import com.elec5619.student.forum.services.NoteCategoryService;
 import com.elec5619.student.forum.services.NoteCommentService;
 import com.elec5619.student.forum.services.NoteService;
 import com.elec5619.student.forum.services.UserService;
+import com.elec5619.student.forum.util.FileUploadUtil;
 import com.elec5619.student.forum.util.JsonReturnType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpSession;
+import java.io.*;
 import java.security.Principal;
 import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/note")
@@ -132,6 +147,58 @@ public class NoteController {
         jsonReturnType.setMessage("");
         return jsonReturnType;
     }
+
+    @PostMapping("/uploadNote")
+    @ResponseBody
+    public JsonReturnType uploadFile(@RequestParam("file") MultipartFile multipartFile,HttpSession httpSession) throws IOException {
+        Random random = new Random();
+        ClassPathResource classPathResource = new ClassPathResource("Notes");
+        JsonReturnType jsonReturnType = new JsonReturnType();
+        int rint = random.nextInt();
+        String path = classPathResource.getFile().getAbsoluteFile() + File.separator + rint;
+        System.out.println(path);
+        System.out.println(classPathResource.exists());
+        File file = new File(path);
+        multipartFile.transferTo(file.getAbsoluteFile());
+        /*classPathResource = new ClassPathResource("Notes" + File.separator + random.nextInt());
+        JsonReturnType jsonReturnType = new JsonReturnType();
+        if(!classPathResource.getFile().exists()){
+            classPathResource.getFile().createNewFile();
+        }
+        multipartFile.transferTo(classPathResource.getFile());
+        jsonReturnType.getData().put("id",classPathResource.getPath());
+        return jsonReturnType;*/
+        jsonReturnType.setMessage(""+rint);
+        return jsonReturnType;
+    }
+
+    @GetMapping("/downloadNote/{id}")
+    @ResponseBody
+    public ResponseEntity<byte[]> downloadNote(HttpSession session,@PathVariable int id) throws IOException {
+        Note note = noteService.findById(id);
+        String name = note.getPath();
+        ClassPathResource classPathResource = new ClassPathResource("Notes");
+        String path = classPathResource.getFile().getAbsoluteFile() + File.separator + name;
+        File file = new File(path);
+        if(file.exists()){
+            System.out.println("file exist");
+        }
+        else {
+            System.out.println("file not exist");
+        }
+        JsonReturnType jsonReturnType = new JsonReturnType();
+        InputStream inputStream = new FileInputStream(file);
+        byte[] bytes = new byte[inputStream.available()];
+        inputStream.read(bytes);
+        MultiValueMap<String,String> header = new HttpHeaders();
+        header.add("Content-Disposition","attachment;filename=" + name);
+        HttpStatus statusCode = HttpStatus.OK;
+        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(bytes,header,statusCode);
+        inputStream.close();
+        return response;
+
+
+   }
 
 
 }
