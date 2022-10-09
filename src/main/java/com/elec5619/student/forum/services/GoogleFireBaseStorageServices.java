4c88ec90535188314ba99bab33696afd9b9ad92a
Zihan Zheng <34792221+ZihanZheng258@users.noreply.github.com>
2022-10-09 15:39:50 +0800

    controller

--- src/main/java/com/elec5619/student/forum/services/GoogleFireBaseStorageServices.java
+++ src/main/java/com/elec5619/student/forum/services/GoogleFireBaseStorageServices.java
@@ -2,17 +2,11 @@ package com.elec5619.student.forum.services;
 
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.storage.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -22,11 +16,25 @@ public class GoogleFireBaseStorageServices {
 
 
     public boolean uploadFile(File file, String fileName, String bucketName) throws IOException {
-        BlobId blobId = BlobId.of(bucketName, fileName);
+        BlobId blobId = BlobId.of("Notepile", fileName);
         BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("media").build();
         Credentials credentials = GoogleCredentials.fromStream(new FileInputStream("src/main/resources/GoogleService/notepile-dc18b-firebase-adminsdk-r6d6r-20cd12a2a9.json"));
         Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
         storage.create(blobInfo, Files.readAllBytes(file.toPath()));
+        return true;
+    }
+
+    public boolean createBucket() throws IOException {
+        Credentials credentials = GoogleCredentials.fromStream(new FileInputStream("src/main/resources/GoogleService/notepile-dc18b-firebase-adminsdk-r6d6r-20cd12a2a9.json"));
+        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+        StorageClass storageClass = StorageClass.COLDLINE;
+        String location = "ASIA";
+        Bucket bucket =
+                storage.create(
+                        BucketInfo.newBuilder("Notepile")
+                                .setStorageClass(storageClass)
+                                .setLocation(location)
+                                .build());
         return true;
     }
 
